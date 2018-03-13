8c05ad646865ef78fa12751b6899f340f61af5b7
Yoann GUILLO <yoann.guillo@geneziis.com>
2018-03-13 11:24:41 +0100

    Change os version to 2016

---
+++ 301-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,3 @@
+﻿New-AzureRmResourceGroup -Name "RG-WEB" -Location "North Europe"
+
+New-AzureRmResourceGroupDeployment -Name "WEBPlatform" -ResourceGroupName "RG-WEB" -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/300-WebPlatform_IIS_SQL/azuredeploy.json" -TemplateParameterUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/300-WebPlatform_IIS_SQL/azuredeploy.parameters.json"
