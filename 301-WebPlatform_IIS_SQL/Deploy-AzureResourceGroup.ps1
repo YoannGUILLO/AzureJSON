ded1d346b128ae29d20e43ad8c6d0d803be45ef2
Yoann GUILLO <yoann.guillo@geneziis.com>
2018-03-13 12:42:43 +0100

    Storage ressources

--- 301-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
+++ 301-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
@@ -1,1 +1,1 @@
-﻿New-AzureRmResourceGroupDeployment -Name "WEBPlatform" -ResourceGroupName "RG-POC1" -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/301-WebPlatform_IIS_SQL/azuredeploy.json"
+﻿New-AzureRmResourceGroupDeployment -Name "WEBPlatformPOC1" -ResourceGroupName "RG-POC1" -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/301-WebPlatform_IIS_SQL/azuredeploy.json"
