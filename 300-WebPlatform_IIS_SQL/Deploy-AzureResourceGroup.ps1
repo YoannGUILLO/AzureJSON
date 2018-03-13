d0561236e9f97a31166f70331151e12670d0b215
Yoann GUILLO <yoann.guillo@geneziis.com>
2018-03-13 16:15:54 +0100

    Update SQL Version to 2014SP2

--- 300-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
+++ 300-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
@@ -1,3 +1,3 @@
-﻿New-AzureRmResourceGroup -Name "RG-POC2" -Location "North Europe"
+﻿New-AzureRmResourceGroup -Name "RG-POC2" -Location "West Europe"
 
 New-AzureRmResourceGroupDeployment -Name "WEBPlatformPOC2" -ResourceGroupName "RG-POC2" -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/300-WebPlatform_IIS_SQL/azuredeploy.json"
