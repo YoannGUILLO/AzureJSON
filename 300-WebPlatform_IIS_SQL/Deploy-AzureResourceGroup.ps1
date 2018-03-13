72f9580ab8b5323b365d8a466e10632ed9dbed4b
Yoann GUILLO <yoann.guillo@geneziis.com>
2018-03-13 15:59:34 +0100

    Change prefix

--- 300-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
+++ 300-WebPlatform_IIS_SQL/Deploy-AzureResourceGroup.ps1
@@ -1,3 +1,3 @@
-﻿#New-AzureRmResourceGroup -Name "RG-WEB" -Location "North Europe"
+﻿New-AzureRmResourceGroup -Name "RG-POC2" -Location "North Europe"
 
-New-AzureRmResourceGroupDeployment -Name "WEBPlatform" -ResourceGroupName "RG-POC1" -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/300-WebPlatform_IIS_SQL/azuredeploy.json"
+New-AzureRmResourceGroupDeployment -Name "WEBPlatformPOC2" -ResourceGroupName "RG-POC2" -TemplateUri "https://raw.githubusercontent.com/YoannGUILLO/AzureJSON/master/300-WebPlatform_IIS_SQL/azuredeploy.json"
